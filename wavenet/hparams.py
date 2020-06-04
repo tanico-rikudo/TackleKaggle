bbad6a0dfb3341e4c5228d304e05a1fc33dd737b
tanico-kazuyo <kazuyo.rikudo@gmail.com>
2020-06-04 21:05:16 +0900

    add dataset preprocessing files

---
+++ wavenet/hparams.py
@@ -0,0 +1,25 @@
+###
+upsample_scales = [4, 8, 8]
+
+seq_len = 10240 #音の長さ
+sampling_rate = 24000
+num_mels = 80 #mel特徴量の次元数を指定
+n_fft = 1024 #FFTのフレームサイズ
+hop_size = 256 #windowがスライドする幅
+window_size = 1024 #windowの幅
+
+learning_rate = 1e-3
+beta_1 = 0.9
+exponential_decay_rate = 0.5
+exponential_decay_steps = 200000
+epoch = 2000
+batch_size = 8
+
+n_test_samples = 1
+save_interval = 50
+
+# 使用するファイルpathを記述
+train_files = "./train_files.txt"
+test_files = "./test_files.txt"
+result_dir = "./result/"
+load_path = None
